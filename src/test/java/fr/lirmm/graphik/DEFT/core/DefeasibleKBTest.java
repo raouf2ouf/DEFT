f105c1ef7c12cff77960847c9fcd93c9f94916ad
Abdelraouf Hecham <hecham.abdelraouf@gmail.com>
2016-08-12 20:23:25 +0200

    Tests: Entailement Tests Setting up Explicit instantiation Tests

--- src/test/java/fr/lirmm/graphik/DEFT/core/DefeasibleKBTest.java
+++ src/test/java/fr/lirmm/graphik/DEFT/core/DefeasibleKBTest.java
@@ -28,21 +28,147 @@ public class DefeasibleKBTest {
 	@Before
 	public void setUp() throws FileNotFoundException, AtomSetException, ChaseException {
 		// Populating first kb via a DLGP File
-		this.kb1 = new DefeasibleKB("./src/test/resources/kowalski.dlgp");
+		this.kb1 = new DefeasibleKB("./src/test/resources/entailment.dlgp");
 		
 		// Populating second kb explicitly
 		this.kb2 = new DefeasibleKB();
 		
-		//----------------- Rules --------------------
-		kb2.addRule("bird(X) :- penguin(X).");
-		kb2.addRule("[DEFT] fly(X) :- bird(X).");
-		kb2.addRule("nofly(X) :- penguin(X).");
-		kb2.addRule("[DEFT] nofly(X) :- penguin(X).");
-		//----------- Negative Constraints -----------
-		kb2.addNegativeConstraint("! :- nofly(X), fly(X).");
-		//----------------- Facts --------------------
-		kb2.addAtom("[DEFT] penguin(kowalski).");
-		kb2.addAtom("bird(tweety).");
+		//----------------- Test1 --------------------
+		// Strict attack (proper defeat). q1(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q1(X) :- p1(X).");
+		kb2.addRule("n1(X) :- neg1(X).");
+		kb2.addNegativeConstraint("! :- n1(X), q1(x).");
+		kb2.addAtom("p1(a).");
+		kb2.addAtom("neg1(a).");
+		
+		//----------------- Test2 --------------------
+		// Defeasible attack (Blocking defeat). q2(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q2(X) :- p2(X).");
+		kb2.addRule("[DEFT] n2(X) :- neg2(X).");
+		kb2.addNegativeConstraint("! :- n2(X), q2(x).");
+		kb2.addAtom("p2(a).");
+		kb2.addAtom("neg2(a).");
+		
+		//----------------- Test3 --------------------
+		// Defeasible attack (proper defeat). q3(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q3(X) :- p3(X).");
+		kb2.addRule("[DEFT] n3(X) :- neg3(X),p3(X).");
+		kb2.addNegativeConstraint("! :- n3(X), q3(x).");
+		kb2.addAtom("p3(a).");
+		kb2.addAtom("neg3(a).");
+		
+		//----------------- Test4 --------------------
+		// Defeasible attack on Strict atom. q4(a) should be STRICTLY_ENTAILED
+		kb2.addRule("q4(X) :- p4(X).");
+		kb2.addRule("[DEFT] n4(X) :- neg4(X),p4(X).");
+		kb2.addNegativeConstraint("! :- n4(X), q4(x).");
+		kb2.addAtom("p4(a).");
+		kb2.addAtom("neg4(a).");
+		
+		//----------------- Test5 --------------------
+		// Defeasible attack (proper defeat) but defended by Strict attack. q5(a) should be DEFEASIBLY_ENTAILED
+		kb2.addRule("[DEFT] q5(X) :- p5(X).");
+		kb2.addRule("[DEFT] n5(X) :- neg5(X),p5(X).");
+		kb2.addRule("d5(X) :- def5(X).");
+		kb2.addNegativeConstraint("! :- n5(X), q5(x).");
+		kb2.addNegativeConstraint("! :- n5(X), d5(x).");
+		kb2.addAtom("p5(a).");
+		kb2.addAtom("neg5(a).");
+		kb2.addAtom("def5(a).");
+		
+		//----------------- Test6 --------------------
+		// Defeasible attack (proper defeat) but defended by Defeasible attack (proper defeat). q6(a) should be DEFEASIBLY_ENTAILED
+		kb2.addRule("[DEFT] q6(X) :- p6(X).");
+		kb2.addRule("[DEFT] n6(X) :- neg6(X),p6(X).");
+		kb2.addRule("d6(X) :- def6(X),n6(X).");
+		kb2.addNegativeConstraint("! :- n6(X), q6(x).");
+		kb2.addNegativeConstraint("! :- n6(X), d6(x).");
+		kb2.addAtom("p6(a).");
+		kb2.addAtom("neg6(a).");
+		kb2.addAtom("def6(a).");
+		
+		//----------------- Test7 --------------------
+		// Defeasible attack (proper defeat) but defended by Defeasible attack (blocking defeat). q7(a) should be DEFEASIBLY_ENTAILED
+		kb2.addRule("[DEFT] q7(X) :- p7(X).");
+		kb2.addRule("[DEFT] n7(X) :- neg7(X),p7(X).");
+		kb2.addRule("[DEFT] d7(X) :- def7(X).");
+		kb2.addNegativeConstraint("! :- n7(X), q7(x).");
+		kb2.addNegativeConstraint("! :- n7(X), d7(x).");
+		kb2.addAtom("p7(a).");
+		kb2.addAtom("neg7(a).");
+		kb2.addAtom("def7(a).");
+		
+		//----------------- Test8 --------------------
+		// Defeasible attack (blocking defeat) but defended by Defeasible attack (proper defeat). q8(a) should be DEFEASIBLY_ENTAILED
+		kb2.addRule("[DEFT] q8(X) :- p8(X).");
+		kb2.addRule("[DEFT] n8(X) :- neg8(X).");
+		kb2.addRule("[DEFT] d8(X) :- def8(X),n8(X).");
+		kb2.addNegativeConstraint("! :- n8(X), q8(x).");
+		kb2.addNegativeConstraint("! :- n8(X), d8(x).");
+		kb2.addAtom("p8(a).");
+		kb2.addAtom("neg8(a).");
+		kb2.addAtom("def8(a).");
+		
+		//----------------- Test9 --------------------
+		// Defeasible attack (blocking defeat) but defended by Defeasible attack (blocking defeat). q9(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q9(X) :- p9(X).");
+		kb2.addRule("[DEFT] n9(X) :- neg9(X).");
+		kb2.addRule("[DEFT] d9(X) :- def9(X).");
+		kb2.addNegativeConstraint("! :- n9(X), q9(x).");
+		kb2.addNegativeConstraint("! :- n9(X), d9(x).");
+		kb2.addAtom("p9(a).");
+		kb2.addAtom("neg9(a).");
+		kb2.addAtom("def9(a).");
+		
+		//----------------- Test10 --------------------
+		// Defeasible attack (proper defeat) but defended by Defeasible attack (proper defeat) but itself
+		// attacked by Defeasible attack (proper defeat). q10(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q10(X) :- p10(X).");
+		kb2.addRule("[DEFT] n10(X) :- neg10(X), p10(X).");
+		kb2.addRule("[DEFT] d10(X) :- def10(X), n10(X).");
+		kb2.addRule("[DEFT] nn10(X) :- att10(X), def10(X).");
+		kb2.addNegativeConstraint("! :- n10(X), q10(x).");
+		kb2.addNegativeConstraint("! :- n10(X), d10(x).");
+		kb2.addNegativeConstraint("! :- d10(X), nn10(x).");
+		kb2.addAtom("p10(a).");
+		kb2.addAtom("neg10(a).");
+		kb2.addAtom("def10(a).");
+		kb2.addAtom("att10(a).");
+		
+		//----------------- Test11 --------------------
+		// Defeasible attack (proper defeat) but defended by Defeasible attack (proper defeat) but 
+		// it is still attacked by Defeasible attack (proper defeat). q11(a) should be NOT_ENTAILED
+		kb2.addRule("[DEFT] q11(X) :- p11(X).");
+		kb2.addRule("[DEFT] n11(X) :- neg11(X), p11(X).");
+		kb2.addRule("[DEFT] d11(X) :- def11(X), n11(X).");
+		kb2.addRule("[DEFT] nn11(X) :- att11(X), def11(X).");
+		kb2.addNegativeConstraint("! :- n11(X), q11(x).");
+		kb2.addNegativeConstraint("! :- n11(X), d11(x).");
+		kb2.addNegativeConstraint("! :- d11(X), nn11(x).");
+		kb2.addAtom("p11(a).");
+		kb2.addAtom("neg11(a).");
+		kb2.addAtom("def11(a).");
+		kb2.addAtom("att11(a).");
+		
+		//----------------- Test12 --------------------
+		// Defeasible attack (proper defeat) but defended by Defeasible attack (proper defeat) but
+		// it is still attacked by Defeasible attack (proper defeat) but defended by Defeasible attack (proper defeat). 
+		// q12(a) should be DEFEASIBLY_ENTAILED
+		kb2.addRule("[DEFT] q12(X) :- p12(X).");
+		kb2.addRule("[DEFT] n12(X) :- neg12(X), p12(X).");
+		kb2.addRule("[DEFT] d12(X) :- def12(X), n12(X).");
+		kb2.addRule("[DEFT] m12(X) :- meg12(X), p12(X).");
+		kb2.addRule("[DEFT] c12(X) :- ctt12(X), m12(X).");
+		kb2.addNegativeConstraint("! :- n12(X), q12(x).");
+		kb2.addNegativeConstraint("! :- n12(X), d12(x).");
+		kb2.addNegativeConstraint("! :- m12(X), q12(x).");
+		kb2.addNegativeConstraint("! :- m12(X), c12(x).");
+		kb2.addAtom("p12(a).");
+		kb2.addAtom("neg12(a).");
+		kb2.addAtom("def12(a).");
+		kb2.addAtom("meg12(a).");
+		kb2.addAtom("ctt12(a).");
+		
 		
 		// Testing if kb1 and kb2 contain the same information
 		kb1.saturate();
@@ -143,15 +269,11 @@ public class DefeasibleKBTest {
 	}
 	
 	@Test
-	public void testDefeasibleKBFileInstantiationEntailementTest1() throws FileNotFoundException, AtomSetException {
+	public void testDefeasibleKBFileInstantiationEntailementTest1() {
 		
 	}
 	
 	/*
-	public void testDefeasibleKBEntailementInExplicitInstantiation() {
-		DefeasibleKB kb = new DefeasibleKB();
-	}
-	
 	public void testDefeasibleKBRuleOrderInFileInstantiation() throws FileNotFoundException, AtomSetException {
 		DefeasibleKB kb = new DefeasibleKB("");
 	}
